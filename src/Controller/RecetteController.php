7a375959c8b74926e3440d0f0f6ada6a528e7bcc
Brault Yoni-Alexandre <yoni-brault@blanche-de-castille.fr>
2023-04-19 13:01:18 +0200

    - Création du contrôleur et de la vue pour les recettes. - Fonctionnalité de faire apparaitre ou pas les recettes privées.

---
+++ src/Controller/RecetteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recette;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecetteController extends AbstractController
+{
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/recette', name: 'app_recette')]
+    public function index(): Response
+    {
+        $recette = $this->entityManager->getRepository(Recette::class)->findAll();
+
+
+
+        return $this->render('recette/index.html.twig', [
+            'recette' => $recette,
+        ]);
+    }
+}
